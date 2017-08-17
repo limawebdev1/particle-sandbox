78aac0b51a49d7d219db8626d54e31cafc592c8c
Lisa Ma <limawebdev@gmail.com>
2017-08-17 12:40:35 -0600

    logging into particle working, getting an access token back

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+
 var Particle = require('particle-api-js');
 var particle = new Particle();
-
+var token;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ //particle login
+
+ particle.login({
+  username: 'lisa@edntech.com',
+  password: process.env.PARTICLE_PASSWORD
+ }).then(
+  function(data) {
+   token = data.body.access_token;
+   console.log(token);
+  },
+  function(err) {
+   console.log('Could not log in.', err);
+  }
+ );
+ res.render('index', {
+  title: 'Express'
+ });
 });
 
 module.exports = router;
